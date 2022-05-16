b80424c1e5d217401e4007d34ff832b9dd7f3376
ikizawa <ikizawas@gmail.com>
2022-05-16 10:43:20 +0900

    test

---
+++ rotate_matrix.py
@@ -0,0 +1,7 @@
+import math
+
+def RotateMatrix(before_x,before_y,d):
+    d = math.radians(d)
+    new_x = before_x*math.cos(d)-before_y*math.sin(d)
+    new_y = before_x*math.sin(d)+before_y*math.cos(d)
+    return new_x,new_y
